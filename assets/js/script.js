7afb3e5660b2c7fe623171d71d8e0783db49c691
Vrentadax <vrentadax@outlook.com>
2022-07-02 15:25:39 -0600

    submit function finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,36 @@
 let searchHistory = []
 let lastCitySearched = ""
 
-let getCityWeather = function(city) {
+let getCityWeather = function (city) {
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=21f70556e9750656e572b49f6f7f3a3b&units=imperial";
 
     fetch(apiUrl)
-        
-        .then(function(response) {
+
+        .then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     displayWeather(data);
                 });
             } else {
                 alert("Error: " + response.statusText);
             }
-        })  
+        })
 
-        .catch(function(error) {
+        .catch(function (error) {
             alert("Unable to connect to OpenWeather");
         })
+};
+
+let searchSubmitHandler = function (event) {
+    event.preventDefault();
+
+    let cityName = $("#cityname").val().trim();
+
+    if (cityName) {
+        getCityWeather(cityName);
+
+        $("#cityname").val("");
+    } else {
+        alert("Please enter a city name");
+    }
 };
