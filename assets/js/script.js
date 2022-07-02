932d70fdf40343b3104236764b4070c35cec5770
Vrentadax <vrentadax@outlook.com>
2022-07-02 17:42:51 -0600

    minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let searchHistory = []
 let lastCitySearched = ""
 
+/**
+ * The function takes a city name as an argument, creates a URL for the OpenWeather API, fetches the
+ * data, and then displays the weather data on the page.
+ * @param city - The name of the city you want to get the weather for.
+ */
 let getCityWeather = function (city) {
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=21f70556e9750656e572b49f6f7f3a3b&units=imperial";
 
@@ -21,6 +26,22 @@ let getCityWeather = function (city) {
         })
 };
 
+/**
+ * The searchSubmitHandler function is called when the user clicks the search button. 
+ * 
+ * The function prevents the default action of the form submit event. 
+ * 
+ * The function then gets the value of the city name input field and trims any leading or trailing
+ * spaces. 
+ * 
+ * If the city name is not empty, the function calls the getCityWeather function and passes the city
+ * name as a parameter. 
+ * 
+ * If the city name is empty, the function displays an alert message. 
+ * 
+ * The function then clears the city name input field.
+ * @param event - The event object is automatically passed to the event handler by the browser.
+ */
 let searchSubmitHandler = function (event) {
     event.preventDefault();
 
@@ -35,6 +56,10 @@ let searchSubmitHandler = function (event) {
     }
 };
 
+/**
+ * It takes the weather data from the API and displays it on the page.
+ * @param weatherData - {
+ */
 let displayWeather = function (weatherData) {
 
     $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
@@ -49,15 +74,15 @@ let displayWeather = function (weatherData) {
                 $("#uv-box").text(data.value);
 
                 if (data.value >= 11) {
-                    $("#uv-box").css("background-color", "#6c49cb")
+                    $("#uv-box").css("background-color", "#FF0000")
                 } else if (data.value < 11 && data.value >= 8) {
-                    $("#uv-box").css("background-color", "#d90011")
+                    $("#uv-box").css("background-color", "#FF6600")
                 } else if (data.value < 8 && data.value >= 6) {
-                    $("#uv-box").css("background-color", "#f95901")
+                    $("#uv-box").css("background-color", "#FF9900")
                 } else if (data.value < 6 && data.value >= 3) {
-                    $("#uv-box").css("background-color", "#f7e401")
+                    $("#uv-box").css("background-color", "#FFFF00")
                 } else {
-                    $("#uv-box").css("background-color", "#299501")
+                    $("#uv-box").css("background-color", "#00FF00")
                 }
             })
         });
@@ -93,6 +118,11 @@ let displayWeather = function (weatherData) {
 
 };
 
+/**
+ * If the city is not in the search history, then add it to the search history and append it to the
+ * search history list.
+ * @param city - The city that the user searched for.
+ */
 let saveSearchHistory = function (city) {
     if (!searchHistory.includes(city)) {
         searchHistory.push(city);
@@ -106,6 +136,9 @@ let saveSearchHistory = function (city) {
     loadSearchHistory();
 };
 
+/**
+ * This function loads the search history from local storage and displays it on the page.
+ */
 let loadSearchHistory = function () {
     searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
     lastCitySearched = JSON.parse(localStorage.getItem("lastCitySearched"));
@@ -128,6 +161,9 @@ let loadSearchHistory = function () {
 
 loadSearchHistory();
 
+/* This is the code that is running when the page loads. It checks to see if there is a city saved in
+local storage. If there is, it runs the getCityWeather function with the saved city. If there is
+not, it does nothing. */
 if (lastCitySearched != "") {
     getCityWeather(lastCitySearched);
 }
